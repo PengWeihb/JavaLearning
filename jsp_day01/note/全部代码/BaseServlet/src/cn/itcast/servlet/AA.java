9e189311a3ea2c54bc6519508e348e0999ad723e
PengWei <1260755167@qq.com>
2019-02-15 01:19:33 +0800

    jsp_day01

---
+++ jsp_day01/note/全部代码/BaseServlet/src/cn/itcast/servlet/AA.java
@@ -0,0 +1,12 @@
+package cn.itcast.servlet;
+
+public class AA {
+	public int sum(int a,int b){
+		System.out.println("AAAAA......2");
+		return a+b;
+	}
+	
+	public int sum(int a,int b,int c){
+		return a+b+c;
+	}
+}
